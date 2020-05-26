ad93f97a199c9b62045397f5c32f68cbf47b323d
b3z3rk_irl <nikhil4709@gmail.com>
2020-05-27 03:37:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 #This uses web scraping and Selenium to scroll the webpage and get more images. Do not close the browser window that pops up,
 #it will close automatically. The images will be labelled 1.jpg, 2.jpg, 3.jpg and so on or by the item names given on Google
 #and will be download in a folder with the same name as what you have searched.
+
 #NOTE:- THIS PROGRAM ASSUMES YOU HAVE CHROME VERSION 83 OR MOZILLA FIREFOX AS YOUR BROWSER AND YOU HAVE THE chromedriver.exe
-# OR THE gecko driver.exe IF YOU HAVE FIREFOX
-#TOO FROM GITHUB INSTEAD OF JUST THIS PROGRAM ELSE IT WILL NOT WORK.
+#OR THE gecko driver.exe IF YOU HAVE FIREFOX .
 
 import warnings
 warnings.filterwarnings("ignore")
