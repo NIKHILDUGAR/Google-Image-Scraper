e7af541f8fdff91f0a99a61e40eece25b3024621
b3z3rk_irl <nikhil4709@gmail.com>
2020-05-26 07:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 #This uses web scraping and Selenium to scroll the webpage and get more images. Do not close the browser window that pops up, 
 #it will close automatically. The images will be labelled 1.jpg,2.jpg,3.jpg and so on or by the item names given on Google 
 #and will be download in a folder with the same name as what you have searched.
-
-
-#NOTE:- THIS PROGRAM ASSUMES YOU HAVE CHROME VERSION 83 AS YOUR BROWSER AND YOU HAVE DOWNLOADED THE ENTIRE REPOSITORY FROM GITHUB
-#INSTEAD OF JUST THIS PROGRAM CAUSE IF YOU JUST COPIED IT WILL NOT WORK.
+#NOTE:- THIS PROGRAM ASSUMES YOU HAVE CHROME VERSION 83 AS YOUR BROWSER AND YOU HAVE THE chromedriver.exe TOO FROM GITHUB INSTEAD OF JUST THIS PROGRAM ELSE IT WILL NOT WORK.
 
 
 import os
